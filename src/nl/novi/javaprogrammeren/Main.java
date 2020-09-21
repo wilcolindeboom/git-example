c78c25d3d60cd8ee093ae56b6465ed11f8637318
Nick Stuivenberg <n.stuivenberg@novi.nl>
2020-09-21 10:13:34 +0200

    Persoonklasse toegevoegd
    Main uitgebreid

--- src/nl/novi/javaprogrammeren/Main.java
+++ src/nl/novi/javaprogrammeren/Main.java
@@ -3,6 +3,10 @@ package nl.novi.javaprogrammeren;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Nick!");
+        System.out.println("Hello Wereld!");
+
+        Person p = new Person("Sjaak");
+
+        System.out.println(p.getName());
     }
 }
